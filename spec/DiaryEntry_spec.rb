ad90547dff4e55e8a2f0f09e79a37f117ce85a0a
Patryk <patrykokpara@hotmail.com>
2022-10-02 21:15:55 +0100

    Passed test for count_words method from  DiaryEntry class

--- spec/DiaryEntry_spec.rb
+++ spec/DiaryEntry_spec.rb
@@ -1,9 +1,13 @@
 require 'DiaryEntry'
 
 RSpec.describe DiaryEntry do 
-    it "" do
+    it "returns title and contents" do
     diary_entry = DiaryEntry.new("First day at school", "It was tough")
     expect(diary_entry.title).to eq "First day at school"
     expect(diary_entry.contents).to eq "It was tough"
+    end
+    it "counts the contents words" do
+    diary_entry = DiaryEntry.new("First day at school", "It was tough")
+    expect(diary_entry.count_words).to eq 3
     end
 end
