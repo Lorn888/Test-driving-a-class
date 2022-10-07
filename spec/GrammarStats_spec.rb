aad375d561d7e437c08185c1d7128a60539bffee
Patryk <patrykokpara@hotmail.com>
2022-10-07 15:03:41 +0100

    Wrote and passed 2nd GrammarStats test

--- spec/GrammarStats_spec.rb
+++ spec/GrammarStats_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe GrammarStats do
     result = grammarStats.check("some text")
     expect(result).to eq false
     end
+
+    it "Rerutns true if text starts with upcase " do
+        grammarStats = GrammarStats.new
+        result = grammarStats.check("Some text")
+        expect(result).to eq true
+    end
+
 end
