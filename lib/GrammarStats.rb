2488094bd14d9aefcee56f318c9d5f9af2eb595d
Patryk <patrykokpara@hotmail.com>
2022-10-11 23:58:05 +0100

    Figured out the method, about to test

--- lib/GrammarStats.rb
+++ lib/GrammarStats.rb
@@ -1,5 +1,7 @@
 class GrammarStats
     def initialize
+      @passed = 0
+      @failed = 0
       # ...
     end
   
@@ -10,17 +12,31 @@ class GrammarStats
      p last_letter = text.split("")[-1]
      
       if first_letter == first_letter.capitalize && (last_letter == "." || last_letter == "!" || last_letter == "?")
-         return true
+        @passed += 1
+        return true
       else 
+         @failed += 1
          return false
       end
     end
   
     def percentage_good
+       percentage =  (100 * @passed)  / (@failed + @passed)
+       return percentage
       # Returns as an integer the percentage of texts checked so far that passed
       # the check defined in the `check` method. The number 55 represents 55%.
     end
   end
   
   ok = GrammarStats.new
-    p ok.check("Some text?")
+  ok.check("Some text")
+  ok.check("Some text")
+  ok.check("Some text")
+  ok.check("Some text")
+  ok.check("Some text")
+  ok.check("Some text?")
+  ok.check("Some text")
+  ok.check("Some text?")
+  ok.check("Some text?")
+  ok.check("Some text?")
+  p ok.percentage_good
