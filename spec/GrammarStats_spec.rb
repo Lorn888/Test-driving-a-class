6ed038c6ef1411ae8a452513dac6b74d7199429f
Patryk <patrykokpara@hotmail.com>
2022-10-12 22:20:35 +0100

    Wrote and passed 3rd  GrammarStats test for percantage_good method

--- spec/GrammarStats_spec.rb
+++ spec/GrammarStats_spec.rb
@@ -49,5 +49,12 @@ RSpec.describe GrammarStats do
       result = grammarStats.percentage_good
       expect(result).to eq "100%"
    end
+   it "Returns 0% if 2 out of 2 texts passed " do
+      grammarStats = GrammarStats.new
+      grammarStats.check("Some text")
+      grammarStats.check("some text?")
+      result = grammarStats.percentage_good
+      expect(result).to eq "0%"
+   end
 
 end
